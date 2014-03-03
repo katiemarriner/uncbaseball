32f6692bdd04187c12505a670ff238d389afbb4e
katiemarriner <kemarriner@gmail.com>
2014-03-03 15:05:07 -0500

    deleted files

--- uncbaseball/wsgi.py
+++ uncbaseball/wsgi.py
@@ -11,7 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uncbaseball.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
 from dj_static import Cling
 
