f40399aa9f4e554c75519b0d94cddc5cd2504f00
katiemarriner <kemarriner@gmail.com>
2014-03-05 14:31:47 -0500

    settings

--- uncbaseball/settings.py
+++ uncbaseball/settings.py
@@ -104,6 +104,7 @@ TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, 'templates'),
 )
 
+'''
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
@@ -123,5 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+'''
 
