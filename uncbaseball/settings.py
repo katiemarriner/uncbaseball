e86e3dfb72cd2495e51838b5f8c6eec77180353a
katiemarriner <kemarriner@gmail.com>
2014-03-05 13:12:35 -0500

    heroku settings added

--- uncbaseball/settings.py
+++ uncbaseball/settings.py
@@ -105,7 +105,7 @@ TEMPLATE_DIRS = (
 )
 
 
-'''
+
 # ADDED FOR HEROKU
 
 import dj_database_url
@@ -121,13 +121,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-'''
 
